8ceb44836c8d1b6892abdc8af1e9bcfdad461fb0
pintraj <pint.raj850@gmail.com>
2020-04-17 04:28:12 +0530

    added login and register page

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <?php
 ?>
-
+<footer class="footer">
+    © 2020 Pintraj
+</footer>
 <script type="text/javascript" src="js/bootstrap.js"></script>
 <script type="text/javascript" src="js/bootstrap.bundle.js"></script>
 <script type="text/javascript" src="js/custom.js"></script>
@@ -9,3 +11,18 @@
 <script type="text/javascript" src="js/validator.js"></script>
 <script type="text/javascript" src="js/waves.js"></script>
 <script type="text/javascript" src="js/widget-data.js"></script>
+<script src="js/jquery.min.js"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="js/jquery.slimscroll.js"></script>
+<!--Wave Effects -->
+<script src="js/waves.js"></script>
+<!--Menu sidebar -->
+<script src="js/sidebarmenu.js"></script>
+<!--stickey kit -->
+<script src="js/sticky-kit.min.js"></script>
+<script src="js/jquery.sparkline.min.js"></script>
+<!--Custom JavaScript -->
+<script src="js/custom.min.js"></script>
