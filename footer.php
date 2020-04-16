2f4dfe29ead502aacb01a49cadee3be68f164400
pintraj <pint.raj850@gmail.com>
2020-04-17 03:25:01 +0530

    added css and js files

--- footer.php
+++ footer.php
@@ -1,1 +1,11 @@
 <?php
+?>
+
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.bundle.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/validation.js"></script>
+<script type="text/javascript" src="js/validator.js"></script>
+<script type="text/javascript" src="js/waves.js"></script>
+<script type="text/javascript" src="js/widget-data.js"></script>
--- header.php
+++ header.php
@@ -1,2 +1,7 @@
 <?php
 ?>
+
+<title>RBR New Website</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/blue.css">
+<link rel="stylesheet" href="css/style.css">
